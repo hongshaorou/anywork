0a9362e134ca56f0005b8b7a73b7575a4f248412
Hongshaorou <apple@Roushan-2.local>
2013-05-08 12:04:11 +0800

    UI and HTML

---
+++ HTML/js/backmanage_login_head.js
@@ -0,0 +1,24 @@
+﻿function focus_txt()
+{
+	if(document.getElementById("id_txt").value== "输入要搜索的内容")
+	{
+		document.getElementById("id_txt").value="";
+		document.getElementById("id_txt").style.color = "#222222";
+	}
+}
+
+function blur_txt()
+{
+	if(document.getElementById("id_txt").value== "")
+	{
+		document.getElementById("id_txt").value="输入要搜索的内容";
+		document.getElementById("id_txt").style.color = "#D9D9D9";
+	}
+}
+
+function click_img_sousou()
+{
+	alert(document.getElementById("id_txt").value);
+}
+	
+
---
+++ HTML/js/backmanage_login_left.js
@@ -0,0 +1,39 @@
+﻿function click_liebiao()
+{
+	if(document.getElementById("id_div_liebiao").style.background = "#4c4c4c")
+	{
+		document.getElementById("id_div_liebiao").style.background = "#4B8DF8";
+		document.getElementById("id_div_xiangmu").style.background = "#4c4c4c";
+		document.getElementById("id_div_tuandui").style.background = "#4c4c4c";
+		document.getElementById("id_div_xuanzhong").style.display="block";
+		document.getElementById("iframe_project_id").style.display="none";
+		document.getElementById("id_div_xuanzhong").style.top = 50 +'px';
+		document.getElementById("iframe_liebiao_id").style.display="block";
+	}
+}
+
+function click_xiangmu()
+{
+	if(document.getElementById("id_div_xiangmu").style.background = "#4c4c4c")
+	{
+		document.getElementById("id_div_xiangmu").style.background = "#4B8DF8";
+		document.getElementById("id_div_liebiao").style.background = "#4c4c4c";
+		document.getElementById("id_div_tuandui").style.background = "#4c4c4c";
+		document.getElementById("id_div_xuanzhong").style.display="block";
+		document.getElementById("iframe_project_id").style.display="block";
+		document.getElementById("id_div_xuanzhong").style.top = 180 +'px';
+		document.getElementById("iframe_liebiao_id").style.display="none";
+	}
+}
+
+function click_tuandui()
+{
+	if(document.getElementById("id_div_tuandui").style.background = "#4c4c4c")
+	{
+		document.getElementById("id_div_xiangmu").style.background = "#4c4c4c";
+		document.getElementById("id_div_liebiao").style.background = "#4c4c4c";
+		document.getElementById("id_div_tuandui").style.background = "#4B8DF8";
+		document.getElementById("id_div_xuanzhong").style.top = 310 +'px';
+
+	}
+}
